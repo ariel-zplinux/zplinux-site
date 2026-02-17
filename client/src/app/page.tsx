50ed3ade89fc94082e43822d8077a2d1babe94af
Ariel Biton <ariel@zplinux.com>
2026-02-17 17:25:28 +0100

    fix: main default page redirect to page selected in url (=> reload OK)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
 import AboutSection from "@/components/sections/About";
+import { redirect, usePathname } from "next/navigation";
 
 export default function ZplinuxHome() {
+  const pathname = usePathname();
+
+  const pagesList = ["/services", "/projects", "/clients", "/contact"];
+
+  if (pagesList.includes(pathname)) {
+    redirect(pathname);
+  }
+
   return <AboutSection />;
 }
